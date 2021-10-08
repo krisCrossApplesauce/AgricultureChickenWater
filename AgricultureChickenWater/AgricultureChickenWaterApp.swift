b60fd5fe37b7ce19c53d5e4d52e9b16f711e10c8
krisCrossApplesauce <karisrr2203@gmail.com>
2021-10-08 12:12:49 -0500

    Initial Commit

---
+++ AgricultureChickenWater/AgricultureChickenWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgricultureChickenWaterApp.swift
+//  AgricultureChickenWater
+//
+//  Created by PMStudent on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AgricultureChickenWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
